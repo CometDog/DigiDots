903e08d2496277c174215278947b4b0a85085fb8
James <james.j.downs@gmail.com>
2015-05-23 16:49:45 -0400

    Added menu icon

--- build/aplite/src/resource_ids.auto.h
+++ build/aplite/src/resource_ids.auto.h
@@ -12,5 +12,6 @@ typedef enum {
   INVALID_RESOURCE = 0,
   RESOURCE_ID_INVALID = 0,
   DEFAULT_MENU_ICON = 0, // Friendly synonym for use in `PBL_APP_INFO()` calls
+  RESOURCE_ID_ICON, // 1
 
 } ResourceId;
