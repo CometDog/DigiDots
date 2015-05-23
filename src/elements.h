e0c84f41d69c959d74aa892e5278d4a2fc1d5784
James <james.j.downs@gmail.com>
2015-05-23 08:51:06 -0400

    Finish face. Start sleep

--- src/elements.h
+++ src/elements.h
@@ -1,2 +1,20 @@
-#include <pebble.h>
-g
+static Window *s_main_window;
+static Layer *s_background_layer, *s_time_layer, *s_sleep_layer;
+
+static int8_t Y;
+static int8_t X;
+static int8_t column;
+static int8_t row[6];
+
+static int8_t hour;
+static int8_t hour_24;
+static int8_t hour1;
+static int8_t hour2;
+static int8_t minute1;
+static int8_t minute2;
+static int8_t second1;
+static int8_t second2;
+
+static bool sleep;
+
+static int8_t bat;
--- src/main.c
+++ src/main.c
@@ -1,2 +1,192 @@
-#include <pebble.h>
-g
+#include "pebble.h"
+#ifdef PBL_COLOR
+  #include "gcolor_definitions.h"
+#endif
+#include "libs/pebble-assist.h"
+#include "elements.h"
+
+static void update_bg(Layer *layer, GContext *ctx) {
+  X = 19;
+  column = 0;
+  #ifdef PBL_COLOR
+    graphics_context_set_fill_color(ctx, GColorDarkGray);
+  #else
+    graphics_context_set_stroke_color(ctx, GColorWhite);
+  #endif
+  
+  row[0] = 1;
+  row[1] = 9;
+  row[2] = 5;
+  row[3] = 9;
+  row[4] = 5;
+  row[5] = 9;
+  
+  if (clock_is_24h_style() == true) {
+    row[0] = 2;
+    row[1] = 4;
+  }
+  
+  while (column < 6) {
+    Y = 125;
+    while (row[column] > 0) {
+      #ifdef PBL_COLOR
+        graphics_fill_circle(ctx, GPoint(X,Y), 5);
+      #else
+        graphics_draw_circle(ctx, GPoint(X,Y), 5);
+      #endif
+      Y -= 15;
+      row[column] -= 1;
+    }
+    column += 1;
+    if (column % 2 == 0) {
+      X += 30;
+    }
+    else {
+      X += 15;
+    }
+  }
+}
+
+static void update_time(Layer *layer, GContext *ctx) {
+  time_t epoch = time(NULL); 
+  struct tm *t = localtime(&epoch);
+  #ifdef PBL_COLOR
+    bat = battery_state_service_peek().charge_percent / 10;
+  #endif
+  
+  hour_24 = t->tm_hour;
+  
+  if (hour_24 >= 22 || hour_24 <= 6) {
+    sleep = true;
+  }
+  else {
+    sleep = false;
+  }
+  
+  if (clock_is_24h_style() == true) {
+    hour = hour_24;
+  }
+  else {
+    hour = hour_24 % 12;
+  }
+  
+  if (clock_is_24h_style() == false && hour == 12) {
+    hour1 = 1;
+    hour2 = 2;
+  }
+  else {
+    hour1 = hour / 10;
+    hour2 = hour - (hour1 * 10);
+  }
+  minute1 = t->tm_min / 10;
+  minute2 = t->tm_min - (minute1 * 10);
+  
+  second1 = t->tm_sec / 10;
+  second2 = t->tm_sec - (second1 * 10);
+  
+  row[0] = hour1;
+  row[1] = hour2;
+  row[2] = minute1;
+  row[3] = minute2;
+  row[4] = second1;
+  row[5] = second2;
+  
+  X = 19;
+  column = 0;
+  #ifdef PBL_COLOR
+  if (bat >= 7) {
+  graphics_context_set_fill_color(ctx, GColorGreen);
+  }
+  else if (bat >= 4) {
+    graphics_context_set_fill_color(ctx, GColorYellow);
+  }
+  else {
+    graphics_context_set_fill_color(ctx, GColorRed);
+  }
+  #else
+    graphics_context_set_fill_color(ctx, GColorWhite);
+  #endif
+  
+  while (column < 6) {
+    Y = 125;
+    while (row[column] > 0) {
+      graphics_fill_circle(ctx, GPoint(X,Y), 5);
+      Y -= 15;
+      row[column] -= 1;
+    }
+    column += 1;
+    if (column % 2 == 0) {
+      X += 30;
+    }
+    else {
+      X += 15;
+    }
+  }
+}
+
+static void update_sleep(Layer *layer, GContext *ctx) {
+  #ifdef PBL_COLOR
+    graphics_context_set_fill_color(ctx, GColorDarkGray);
+  #else
+    graphics_context_set_stroke_color(ctx, GColorWhite);
+  #endif
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  layer_mark_dirty(s_time_layer);
+}
+
+static void bt_handler(bool connected) {
+  if (connected) {
+    vibes_short_pulse();
+  }
+  else {
+    vibes_double_pulse();
+  }
+}
+
+static void main_window_load(Window *window) {
+  GRect bounds = GRect(0,19,144,168);
+  
+  window_set_background_color(window, GColorBlack);
+  
+  s_background_layer = layer_create(bounds);
+  s_time_layer = layer_create(bounds);
+  s_sleep_layer = layer_create(bounds);
+  layer_set_update_proc(s_background_layer, update_bg);
+  layer_set_update_proc(s_time_layer, update_time);
+  
+  if (sleep == true) {
+    layer_set_update-proc(s_sleep_layer, update_sleep);
+  }
+  
+  layer_add_to_window(s_background_layer, window);
+  layer_add_to_window(s_time_layer, window);
+  layer_add_to_window(s_sleep_layer, window);
+}
+
+static void main_window_unload(Window *window) {
+  layer_destroy_safe(s_background_layer);
+  layer_destroy_safe(s_time_layer);
+  layer_destroy_safe(s_sleep_layer);
+}
+
+static void init() {
+  s_main_window = window_create();
+  window_handlers(s_main_window, main_window_load, main_window_unload);
+  window_stack_push(s_main_window, true);
+  
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  bluetooth_connection_service_subscribe(bt_handler);
+}
+
+static void deinit() {
+  bluetooth_connection_service_unsubscribe();
+  window_destroy_safe(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
